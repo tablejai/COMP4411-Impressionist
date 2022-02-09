bbb8fd6ebced26210795c4d65a1c5d4a33dcc014
Tablejai <alexchan815@gmail.com>
2022-02-09 19:31:53 +0800

    Revert "Auto stash before merge of "chalex_branch" and "origin/chalex_branch""
    This reverts commit 423aa2e19d0828a467a06376bed85ff1598eea6e.

--- PaintView.cpp
+++ PaintView.cpp
@@ -11,22 +11,14 @@
 #include "ImpBrush.h"
 #include <iostream>
 using namespace std;
-
-
-
-
-#define LEFT_MOUSE_DOWN		1
-
-#define LEFT_MOUSE_DRAG		2
-
-#define LEFT_MOUSE_UP		3
-
-#define RIGHT_MOUSE_DOWN	4
-
-#define RIGHT_MOUSE_DRAG	5
-
-#define RIGHT_MOUSE_UP		6
-
+
+
+#define LEFT_MOUSE_DOWN		1
+#define LEFT_MOUSE_DRAG		2
+#define LEFT_MOUSE_UP		3
+#define RIGHT_MOUSE_DOWN	4
+#define RIGHT_MOUSE_DRAG	5
+#define RIGHT_MOUSE_UP		6
 
 
 #define LEFT_MOUSE_DOWN		1
@@ -212,17 +204,6 @@ int PaintView::handle(int event)
 		//if(coord.x>0 && coord.x<= m_nWindowWidth && coord.y>0 && coord.y<= m_nWindowHeight)
 			m_pDoc->m_pCursor->setpos(coord.x,coord.y);
 		m_pDoc->m_pUI->m_origView->triggerupdate();
-		break;
-
-	case FL_MOVE:
-
-		coord.x = Fl::event_x();
-
-		coord.y = Fl::event_y();
-		//if(coord.x>0 && coord.x<= m_nWindowWidth && coord.y>0 && coord.y<= m_nWindowHeight)
-		m_pDoc->m_pCursor->setpos(coord.x,coord.y);
-		m_pDoc->m_pUI->m_origView->triggerupdate();
-
 		break;
 	default:
 		return 0;

9e8f2dc16c2d0d94c9027bc9f8240407c5c575c1
Tablejai <alexchan815@gmail.com>
2022-02-26 22:31:38 +0800

    Denoising for Edge Detection

--- KernelBrush.cpp
+++ KernelBrush.cpp
@@ -48,7 +48,7 @@ void KernelBrush::BrushMove(const Point source, const Point target)
 			}
 			//std::cout << x2 << " " << y2 << std::endl;
 			vector<int> outputVal = Kernel(Point(x1, y1), Point(x2, y2));
-			KernelSetColor(Point(x2,y2), outputVal[0], outputVal[1], outputVal[2], false, true);
+			KernelSetColor(Point(x2,y2), outputVal[0], outputVal[1], outputVal[2], 0, true);
 		}
 	}
 	 dlg->m_paintView->RestorePreviousDataRGBA(dlg->m_paintView->rgbaBrush, GL_BACK);
@@ -87,7 +87,7 @@ vector<int> KernelBrush::Kernel(const Point source, const Point target) {
 
 }
 
-void KernelBrush::KernelSetColor(const Point target, const int r, const int g, const int b, bool dealWithEdge, bool has_alpha) {
+void KernelBrush::KernelSetColor(const Point target, const int r, const int g, const int b, int tarArr, bool has_alpha) {
 	ImpressionistDoc* pDoc = GetDocument();
 	ImpressionistUI* dlg = pDoc->m_pUI;
 
@@ -99,17 +99,21 @@ void KernelBrush::KernelSetColor(const Point target, const int r, const int g, c
 	int r_mult = colorChooser->r();
 	int g_mult = colorChooser->g();
 	int b_mult = colorChooser->b();
-	if (!dealWithEdge) {
+	if (tarArr == 0) {
 		dlg->m_paintView->rgbaBrush[(target.x + target.y * w) * 4] = min(max(r, 0), 255) * r_mult;
 		dlg->m_paintView->rgbaBrush[(target.x + target.y * w) * 4 + 1] = min(max(g, 0), 255) * g_mult;
 		dlg->m_paintView->rgbaBrush[(target.x + target.y * w) * 4 + 2] = min(max(b, 0), 255) * b_mult;
 		dlg->m_paintView->rgbaBrush[(target.x + target.y * w) * 4 + 3] = alpha * 255;
 	}
-	else {
+	else if(tarArr == 1) {
 		pDoc->m_edgeView[(target.x + target.y * w) * 3] = min(max(r, 0), 255) * r_mult;
 		pDoc->m_edgeView[(target.x + target.y * w) * 3 + 1] = min(max(g, 0), 255) * g_mult;
 		pDoc->m_edgeView[(target.x + target.y * w) * 3 + 2] = min(max(b, 0), 255) * b_mult;
-
+	}
+	else if (tarArr == 2) {
+		denoisedImg[(target.x + target.y * w) * 3] = min(max(r, 0), 255);
+		denoisedImg[(target.x + target.y * w) * 3 + 1] = min(max(g, 0), 255);
+		denoisedImg[(target.x + target.y * w) * 3 + 2] = min(max(b, 0), 255);
 	}
 }
 
@@ -124,6 +128,7 @@ void KernelBrush::SobelOperator(const Point source, const Point target) {
 		delete [] pDoc->m_edgeView;
 	}
 	pDoc->m_edgeView = new unsigned char[w * h * 4];
+	Denoise(source, target);
 
 	if (pDoc == NULL) {
 		printf("PointBrush::BrushMove  document is NULL\n");
@@ -153,14 +158,45 @@ void KernelBrush::SobelOperator(const Point source, const Point target) {
 			setVal[1] = min(max(setVal[1], 0), 255);
 			setVal[2] = min(max(setVal[2], 0), 255);
 
-			KernelSetColor(Point(x2, y2), setVal[0], setVal[1], setVal[2], true, false);
+			KernelSetColor(Point(x2, y2), setVal[0], setVal[1], setVal[2], 1, false);
 		}
 	}
 	dlg->m_origView->showEdge = true;
 	dlg->m_origView->refresh();
 }
 
+void KernelBrush::Denoise(const Point source, const Point target) {
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
 
+	int w = pDoc->m_nPaintWidth;
+	int h = pDoc->m_nPaintHeight;
+	if (denoisedImg == nullptr) {
+		delete[] denoisedImg;
+	}
+
+	if (pDoc == NULL) {
+		printf("PointBrush::BrushMove  document is NULL\n");
+		return;
+	}
+	vector<vector<float>> meanFilter = { {1/9, 1/9, 1/9}, {1/9,1/9,1/9}, {1/9,1/9,1/9} };
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			int x1 = source.x + i - size / 2;
+			int x2 = target.x + i - size / 2;
+			int y1 = source.y + j - size / 2;
+			int y2 = target.y + j - size / 2;
+			if (x1 > (w - 1) || x2 < 0 || y1 >(h - 1) || y2 < 0) {
+				continue;
+			}
+			kernel = meanFilter;
+			vector<int> outputVal = Kernel(Point(x1, y1), Point(x2, y2));
+
+			KernelSetColor(Point(x2, y2), outputVal[0], outputVal[1], outputVal[2], 0, false);
+		}
+	}
+}
 
 void KernelBrush::BrushEnd(const Point source, const Point target)
 {
--- KernelBrush.h
+++ KernelBrush.h
@@ -10,14 +10,16 @@ class KernelBrush : public ImpBrush
 
 public:
 	int size = 10;
+	unsigned char* denoisedImg = nullptr;
 	std::vector<std::vector<float>> kernel;
 	KernelBrush(ImpressionistDoc* pDoc = NULL, char* name = NULL);
 	void BrushBegin(const Point source, const Point target);
 	void BrushMove(const Point source, const Point target);
 	void BrushEnd(const Point source, const Point target);
 	vector<int> Kernel(const Point source, const Point target);
-	void KernelSetColor(const Point target, const int r, const int g, const int b, bool ,bool);
+	void KernelSetColor(const Point target, const int r, const int g, const int b, int ,bool);
 	void SobelOperator(const Point, const Point);
+	void Denoise(const Point, const Point);
 	char* BrushName(void);
 };
 
